cdcee28b3cf059c2b4f6d9be3550fe1f0f1ebf83
Monika Osiak <m.osiak@samsung.com>
2019-10-28 14:43:34 +0100

    Add parser for args and support for POST method during register

--- resources.py
+++ resources.py
@@ -1,10 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import make_response, render_template
 
 users = [
     'osiakm',
     'admin'
 ]
+
+parser = reqparse.RequestParser().add_argument(
+    'username', help='This field cannot be blank', required=True
+).add_argument(
+    'email', help='This field cannot be blank', required=True
+).add_argument(
+    'password', help='This field cannot be blank', required=True
+)
 
 class Index(Resource):
     def get(self):
@@ -22,6 +30,12 @@ class Register(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('register.html'), 200, headers)
+
+    def post(self):
+        headers = {'Content-Type': 'text/html'}
+        data = parser.parse_args()
+        print(data)
+        return make_response(data, 200, headers)
 
 
 class CheckLogin(Resource):
