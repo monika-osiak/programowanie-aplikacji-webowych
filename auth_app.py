a5a57504b7cfee53afd390139c47c5980a5ba297
Monika Osiak <m.osiak46@gmail.com>
2019-11-15 20:24:12 +0100

    Hardcode users and session dict

--- auth_app.py
+++ auth_app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
+users = {
+    'admin': 'password',
+}
+
+session = dict()
+
 @app.route('/') # main page
 def index():
     pass
